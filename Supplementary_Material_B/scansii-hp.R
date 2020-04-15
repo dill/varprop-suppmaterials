e15059df07f8c8f59b960fa83ae70db187563a2f
David Lawrence Miller <github@ninepointeightone.net>
2020-04-15 21:33:34 +0100

    consolidate hp analysis

--- Supplementary_Material_B/scansii-hp.R
+++ Supplementary_Material_B/scansii-hp.R
@@ -9,34 +9,14 @@ library(plyr)
 library(ggplot2)
 library(gridExtra)
 library(viridis)
-source("R/make_ss_fs_data.R")
-source("R/est_N_var_ss.R")
+source("make_ss_fs_data.R")
+source("est_N_var_ss.R")
 
 
 ## 1. data setup
 
-# load the SCANS-II data
-load("data/SCANSII.RData")
-
-# select the area we are interested in:
-#   R is coastal western Ireland
-#   O is the Irish sea
-#   N is Western coastal Scotland
-area_of_interest <- c("N", "R", "O")
-air.ddf <- air.ddf[air.ddf$Stratum %in% area_of_interest, ]
-air.segments <- air.segments[air.segments$Stratum %in% area_of_interest, ]
-
-# select the harbour porpoise observations only
-air.ddf <- air.ddf[air.ddf$Species=="ppho", ]
-
-# only include segments with beaufort <= max.bf
-max.bf <- 4
-air.segments <- air.segments[air.segments$Beaufort <= max.bf, ]
-air.ddf <- air.ddf[air.ddf$Beaufort <= max.bf, ]
-
-# shortcut names
-ddf_data <- air.ddf
-segments_data <- air.segments
+# load the harbour porpoise data
+load("hp.RData")
 
 # bin Beaufort
 ddf_data$beaufort <- cut(ddf_data$Beaufort, c(0,1,2,4), include.lowest=TRUE)
@@ -80,7 +60,7 @@ df_fs_chop <- dsm(count ~ s(x, y, size_class, k=20, bs="fs"),
                   observation.data = ddf_data_ss, family=tw(a=1.2))
 
 # how well does the model do?
-source("R/obs_exp.R")
+source("obs_exp.R")
 print(obs_exp(df_fs_chop, ~beaufort))
 
 
@@ -95,11 +75,6 @@ print(obs_exp(dsm_boring, ~beaufort))
 
 
 ## 6. set up prediction data
-load("data/airgrid.RData")
-pred <- pred_grid[pred_grid$Stratum %in% area_of_interest, ]
-
-# coastline for plotting
-coastline <- map_data("world", c("UK", "Ireland", "Isle of Man"))
 
 # prediction grid, three times
 pp_ss <- rbind(pred, pred, pred)
@@ -193,8 +168,7 @@ plist[["comb"]] <- ggplot(comb_plot) +
   geom_tile(aes(x=Longitude, y=Latitude, fill=Abundance_t),
             width=0.033, height=0.066)+
   geom_polygon(data = coastline, aes(x=long, y = lat, group = group), fill="#D2D2D2") +
-  coord_quickmap(#projection="mercator",
-            ylim=range(this_plot$Latitude), xlim=range(this_plot$Longitude)) +
+  coord_quickmap(ylim=range(this_plot$Latitude), xlim=range(this_plot$Longitude)) +
   scale_fill_viridis()+#comb_boring_fill_scale)+
   geom_segment(aes(x=st.lon, y=st.lat, xend=end.lon, yend=end.lat), alpha=0.4,
                data=segments_data) +
@@ -209,15 +183,13 @@ plist[["comb"]] <- ggplot(comb_plot) +
                           legend.key.height=unit(10, "pt"),
                           legend.position="bottom")
 
-# test the figure that goes into the paper
-#pdf("test.pdf", width=8, height=8)
-#grid.arrange(grobs=list(plist[[2]],
-#                        plist[[3]],
-#                        plist[[1]],
-#                        plist[["comb"]]), nrow=2, ncol=2)
-#dev.off()
+# figure that goes into the paper
+grid.arrange(grobs=list(plist[[2]],
+                        plist[[3]],
+                        plist[[1]],
+                        plist[["comb"]]), nrow=2, ncol=2)
 
-## now plot estimated abundance model
+# add plot of estimated abundance model
 #plist[["boring"]] <- ggplot(boring_plot) +
 #  geom_tile(aes(x=Longitude, y=Latitude, fill=Abundance),
 #            width=0.033, height=0.066)+
@@ -235,7 +207,7 @@ plist[["comb"]] <- ggplot(comb_plot) +
 #                          legend.position="bottom")
 
 
-## plot everything -- check before paper
+## plot everything
 # switching coord_map to coord_quickmap above makes the wait more
 #  palatable
 #grid.arrange(grobs=list(plist[[2]],
@@ -243,9 +215,9 @@ plist[["comb"]] <- ggplot(comb_plot) +
 #                        plist[[1]],
 #                        plist[["comb"]],
 #                        plist[["boring"]]), nrow=2, ncol=3)
-#
 
-## 10. save output
+
+## 10. save output to load into paper
 save(plist, df_s, df_fs_chop, vp, ests, ests_vg, dsm_boring, var_gam,
      file="hp-output.RData")
 
